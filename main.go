2f5852631d44472c85321241bf06e08692cff974
peterjidamva <jidamvap@gmail.com>
2022-03-10 19:46:48 +0300

    adding stuffs

--- main.go
+++ main.go
@@ -6,18 +6,39 @@ import (
 	rvsStr "mypackgs"
 )
 
-//using a package 
+//structure
+type Student struct {
+	Name  string
+	regNo string
+	age   int
+	sex   string
+
+	// CalculateReg();
+
+}
+
+func CalculateReg(s Student) int {
+
+	value := len(s.Name)
+
+	return value
+}
+
+//using a package
 func tellingFacts(word string) string {
 	return word
 }
 
 func main() {
-	 //variable declarations
+	//structure data filling
+
+	student1 := Student{Name:"PETER Jidamva" ,regNo:"2000-04-1234",age:21,sex:"M" }
+	
+	//variable declarations
 	var uname = "DRIFT"
 	var time = 12
 	const pi = 3.14
 	var array [3]string
-    
 
 	//arrays
 	array[0] = "drift"
@@ -29,6 +50,9 @@ func main() {
 	//also we can declare variable as
 
 	value := 6.55
+	
+	fmt.Println(CalculateReg(student1))
+
 	fmt.Println(array)
 	fmt.Println(number[1:4])
 	fmt.Println(tellingFacts("drift"))
@@ -40,7 +64,7 @@ func main() {
 
 	x := 3
 	y := 8
-//control statements
+	//control statements
 	if x <= y {
 		fmt.Printf("%d is less than %d", x, y)
 
@@ -49,44 +73,41 @@ func main() {
 
 	}
 
-   switch x {
-     
-   case 3:
-	fmt.Println("it is 3")
-   case 5:
-	fmt.Println("it is 5")
-   default:
-	 fmt.Println("we dont know it")
-   }
+	switch x {
 
-   i:= 0 
-   
-   for i<= 10 {
-	   fmt.Println(i)
-	   i++
-   }
+	case 3:
+		fmt.Println("it is 3")
+	case 5:
+		fmt.Println("it is 5")
+	default:
+		fmt.Println("we dont know it")
+	}
 
+	i := 0
 
-   //maps 
-   
-   email := make(map[string]string)
-   email["jidamva"] = "jidamva@gmail.com"
-   email["drift"] = "drift@gmail.com"
-   email["name"] = "name@gmail.com"
-   
-   fmt.Println(email)
-   fmt.Println(len(email))
-   delete(email,"drift")
-   fmt.Println(email["name"])
+	for i <= 10 {
+		fmt.Println(i)
+		i++
+	}
 
-   //declaring and initializing a map
-school := map[int] string {
-1 : "money",
-2 : "fame",
-3 : "Power"}
+	//maps
 
-fmt.Println(school)
+	email := make(map[string]string)
+	email["jidamva"] = "jidamva@gmail.com"
+	email["drift"] = "drift@gmail.com"
+	email["name"] = "name@gmail.com"
 
+	fmt.Println(email)
+	fmt.Println(len(email))
+	delete(email, "drift")
+	fmt.Println(email["name"])
 
+	//declaring and initializing a map
+	school := map[int]string{
+		1: "money",
+		2: "fame",
+		3: "Power"}
+
+	fmt.Println(school)
 
 }
